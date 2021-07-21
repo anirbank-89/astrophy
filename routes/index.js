788c5a3c5999d3784a8f4ec2afe089c72a3cc86b
Arindam Samanta <asamanta748@gmial.com>
2021-07-21 13:03:27 +0530

    up

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+  return res.send({status:true})
+});
+
 router.use('/v1',require('./v1'))
 module.exports = router;
